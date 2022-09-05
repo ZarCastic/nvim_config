e2219e1120be6507ca040dbd10e8c466e4d1ff97
Tobias Fuchs <tobias.fuchs@kinexon.com>
2022-09-05 08:37:39 +0200

    add all to treesitter

--- lua/treesitter-config.lua
+++ lua/treesitter-config.lua
@@ -1,5 +1,5 @@
 require("nvim-treesitter.configs").setup({
-	ensure_installed = { "c", "cpp", "lua", "rust", "javascript", "cmake", "css", "go", "yaml", "typescript", "vue", "bash", "html", "proto", "python"},
+	ensure_installed = all,
 	sync_install = false,
 	auto_install = true,
 	highlight = {
